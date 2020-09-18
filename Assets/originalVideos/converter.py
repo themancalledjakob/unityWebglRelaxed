3891e31fd3208185678eedc62b73fda361aee355
themancalledjakob <studio@pointer.click>
2020-09-18 15:27:01 +0200

    add originalVideo directory

---
+++ Assets/originalVideos/converter.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import json
+import os
+import os.path
+import subprocess
+import sys
+from pathlib import Path
+import argparse
+
+parser = argparse.ArgumentParser(description='Doing things..')
+parser.add_argument('--input', default='.', help='input directory')
+parser.add_argument('--output', default='.', help='output directory')
+
+args = parser.parse_args()
+
+inputDir = args.input
+outputDir = args.output
+pathlist = Path(inputDir).glob('./*.*')
+videos = []
+
+print(outputDir)
+print(inputDir)
+
+for path in pathlist:
+     # because path is object not string
+     path_in_str = str(path)
+
+     if (path_in_str.endswith(".json")):
+         continue
+
+     if (path_in_str.endswith(".py")):
+         continue
+
+     if (path_in_str.endswith(".meta")):
+         continue
+
+     filename = os.path.basename(path_in_str)
+     (basename, ext) = os.path.splitext(filename)
+     outname = f"{basename}.webm"
+     tmpname = f"{basename}_tmp.webm"
+     i = path_in_str
+     o = f"{outputDir}/{outname}"
+     t = f"{outputDir}/{tmpname}"
+     my_file = Path(o)
+     if not my_file.is_file():
+        infoString = subprocess.check_output(["ffprobe","-v","quiet","-print_format","json","-show_format","-show_streams",f"{i}"])
+        print("try loading")
+        print(basename)
+        print(infoString)
+        info = json.loads(infoString)
+        print("loaded")
+        print(info)
+        if "streams" in info and "width" in info["streams"][0] and int(info["streams"][0]["width"]) > 1920:
+            subprocess.run(["ffmpeg","-i",f"{i}","-vf","scale=1920:-1","-c:v","libvpx","-b:v","1M","-c:a","libvorbis",f"{t}"])
+        else:
+            subprocess.run(["ffmpeg","-i",f"{i}","-c:v","libvpx","-b:v","1M","-c:a","libvorbis",f"{t}"])
+        subprocess.run(["mv",f"{t}",f"{o}"])
+        # subprocess.run(["rm",f"{i}"])
+     videos.append(outname)
+
+data = {
+    'distanceThreshold': 8.0,
+    'videos':videos
+}
+
+with open(f"{outputDir}/settings.json", 'w') as outfile:
+    json.dump(data, outfile)
